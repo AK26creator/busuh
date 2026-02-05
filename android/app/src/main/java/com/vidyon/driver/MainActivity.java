0f3301d1660708777ae72d0c18b484acf46610e9
Kamalesh <206804734+rithikajoshi0-0@users.noreply.github.com>
2026-02-05 11:40:46 +0530

    feat: Add Leaflet mapping library with React-Leaflet and Capacitor Geolocation plugin, updating `package.json` and generating associated Android build artifacts.

--- android/app/src/main/java/com/vidyon/driver/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.vidyon.driver;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.vidyon.driver',
-  appName: 'vidyon-driver',
-  webDir: 'dist'
-};
-
-export default config;
